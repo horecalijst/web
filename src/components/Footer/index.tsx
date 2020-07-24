e8d272ad1f463b547cbc719ac95697e2bcf94c23
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-25 00:28:19 +0200

    :sparkles: Footer links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,23 @@
+import Link from 'next/link';
 import React from 'react';
 
 import styles from './styles.module.css';
 
 const Footer = () => (
-  <footer className={styles.footer}>&copy; 2020 Horecalijst</footer>
+  <footer className={styles.footer}>
+    <p>&copy; 2020 Horecalijst</p>
+    <nav>
+      <Link href="/gebruiksvoorwaarden">
+        <a>Gebruiksvoorwaarden</a>
+      </Link>
+      <Link href="/privacy">
+        <a>Privacy</a>
+      </Link>
+      <Link href="/contact">
+        <a>Contact</a>
+      </Link>
+    </nav>
+  </footer>
 );
 
 export default Footer;
