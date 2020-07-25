efb9923d3a6d83eee8cb25657138d4fc879fe04c
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-25 23:47:55 +0200

    :lipstick: Bold

--- src/components/Pages/Landing/PrivacyDisclaimer/index.tsx
+++ src/components/Pages/Landing/PrivacyDisclaimer/index.tsx
@@ -4,9 +4,9 @@ import styles from './styles.module.css';
 
 const PrivacyDisclaimer = () => (
   <div className={styles.privacyDisclaimer}>
-    ⚠️ Privacy is belangrijk, dat vinden wij ook. Jouw gegevens worden veilig
-    opgeslagen en worden enkel gedeeld met bevoegde autoriteiten in het kader
-    van contactonderzoek.{' '}
+    ⚠️ Privacy is belangrijk, dat vinden wij ook. Jouw gegevens worden{' '}
+    <strong>veilig opgeslagen</strong> en worden enkel gedeeld met bevoegde
+    autoriteiten in het kader van contactonderzoek.{' '}
     <strong>
       Na 14 dagen worden jouw gegevens ook steeds definitief verwijderd.
     </strong>
