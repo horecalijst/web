de2db72527aaf508da9a7036b4e70442c10c767a
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-25 09:23:45 +0200

    :mag: More SEO entries

--- src/api/sitemap.ts
+++ src/api/sitemap.ts
@@ -13,6 +13,10 @@ export default async (_req: NextApiRequest, res: NextApiResponse) => {
 
   // static
   sitemap.write({ url: '/', priority: 1 });
+  sitemap.write({ url: '/mijn-zaak', priority: 0.8 });
+  sitemap.write({ url: '/contact', priority: 0.8 });
+  sitemap.write({ url: '/gebruiksvoorwaarden', priority: 0.5 });
+  sitemap.write({ url: '/privacy', priority: 0.5 });
 
   sitemap.end();
 
