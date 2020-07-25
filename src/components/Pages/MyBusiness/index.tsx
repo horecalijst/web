5579856e8325fc474c3eab431f548f2b2c397e9f
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-25 10:47:32 +0200

    :sparkles: Sign up/in functionality

--- src/components/Pages/MyBusiness/index.tsx
+++ src/components/Pages/MyBusiness/index.tsx
@@ -1,37 +1,94 @@
+import { gql, useMutation } from '@apollo/client';
 import Footer from 'components/Footer';
 import Form from 'components/Form';
 import Header from 'components/Header';
 import Layout from 'components/Layout';
 import Meta from 'components/Meta';
-import React from 'react';
+import React, {
+  ChangeEvent,
+  FormEvent,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 
 import styles from './styles.module.css';
 
-const MyBusiness = () => (
-  <Layout>
-    <Meta title="Mijn zaak" />
-    <Header />
-    <Layout.Content>
-      <div className={styles.myBusiness}>
-        <h2>Mijn zaak</h2>
-        <p>
-          Meld je aan en probeer het nu gratis. Eens u bent aangemeld kan u 1 of
-          meerdere zaken toevoegen &amp; beheren.
-        </p>
-        <br />
-        <Form className={styles.form}>
-          <Form.Field>
-            <Form.Label htmlFor="email">Email</Form.Label>
-            <Form.Input id="email" name="email" type="email" />
-          </Form.Field>
-          <Form.Field>
-            <Form.Button type="submit">Aanmelden</Form.Button>
-          </Form.Field>
-        </Form>
-      </div>
-    </Layout.Content>
-    <Footer />
-  </Layout>
-);
+const MyBusiness = () => {
+  const [createAuthenticationRequest, { loading, data }] = useMutation(gql`
+    mutation createAuthenticationRequest($email: String!) {
+      createAuthenticationRequest(email: $email)
+    }
+  `);
+  const [email, setEmail] = useState('');
+  const [didSubmit, setDidSubmit] = useState(false);
+  const didSubmitSuccesfully = useMemo(() => {
+    if (didSubmit !== true) {
+      return false;
+    }
+
+    return data?.createAuthenticationRequest === true;
+  }, [data?.createAuthenticationRequest, didSubmit]);
+
+  const onSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      setDidSubmit(true);
+
+      createAuthenticationRequest({ variables: { email } }).catch(() => {
+        alert('Er ging iets mis, probeer het later eens opnieuw.');
+        setDidSubmit(false);
+      });
+    },
+    [createAuthenticationRequest, email],
+  );
+
+  useEffect(() => {
+    if (didSubmitSuccesfully) {
+      alert('We hebben jou een mailtje gestuurd! Hiermee kan je inloggen.');
+
+      setDidSubmit(false);
+    }
+  }, [didSubmitSuccesfully]);
+
+  return (
+    <Layout>
+      <Meta title="Mijn zaak" />
+      <Header />
+      <Layout.Content>
+        <div className={styles.myBusiness}>
+          <h2>Mijn zaak</h2>
+          <p>
+            Meld je aan en probeer het nu gratis. Eens u bent aangemeld kan u 1
+            of meerdere zaken toevoegen &amp; beheren.
+          </p>
+          <br />
+          <Form className={styles.form} onSubmit={onSubmit}>
+            <Form.Field>
+              <Form.Label htmlFor="email">Email</Form.Label>
+              <Form.Input
+                id="email"
+                name="email"
+                type="email"
+                value={email}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  setEmail(e.target.value)
+                }
+              />
+            </Form.Field>
+            <Form.Field>
+              <Form.Button type="submit" isLoading={loading}>
+                Aanmelden
+              </Form.Button>
+            </Form.Field>
+          </Form>
+        </div>
+      </Layout.Content>
+      <Footer />
+    </Layout>
+  );
+};
 
 export default MyBusiness;
