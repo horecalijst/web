a52e4db017b496d915308a34acbc24c4181d4a55
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-28 19:03:00 +0200

    :fire: Remove PhoneInput

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -5,7 +5,6 @@ import Field from './Field';
 import Input from './Input';
 import InputPrefix from './InputPrefix';
 import Label from './Label';
-import PhoneInput from './PhoneInput';
 import Select from './Select';
 
 const Form = (props: FormHTMLAttributes<HTMLFormElement>) => {
@@ -16,7 +15,6 @@ Form.Field = Field;
 Form.Label = Label;
 Form.InputPrefix = InputPrefix;
 Form.Input = Input;
-Form.PhoneInput = PhoneInput;
 Form.Select = Select;
 Form.Button = Button;
 
