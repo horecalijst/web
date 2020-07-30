8ca3f06dd4089aad508a9a5dcd90842d976447af
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-30 11:55:26 +0200

    :hammer_and_wrench: Apparently autoprefixer is deprecated

--- postcss.config.js
+++ postcss.config.js
@@ -3,8 +3,5 @@ module.exports = {
     tailwindcss: {},
     precss: {},
     'postcss-preset-env': {},
-    autoprefixer: {
-      browsers: ['last 2 versions', 'iOS >= 8', 'Safari >= 8'],
-    },
   },
 };
