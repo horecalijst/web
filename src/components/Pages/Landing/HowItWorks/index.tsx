f4b51a5a3baec0392df96a99add9c7e798bcf1e4
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-30 15:13:52 +0200

    :sparkles: Use dynamic pricing

--- src/components/Pages/Landing/HowItWorks/index.tsx
+++ src/components/Pages/Landing/HowItWorks/index.tsx
@@ -1,32 +1,51 @@
+import { gql, useQuery } from '@apollo/client';
 import Link from 'next/link';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import styles from './styles.module.css';
 
-const HowItWorks = () => (
-  <div className={styles.howItWorks}>
-    <h2 id="hoe-het-werkt">Hoe werkt het?</h2>
-    <p>
-      Voor u als horeca-uitbater is het bijhouden van de contactgegevens van uw
-      klanten de nieuwste in een steeds langer wordende lijst van
-      coronamaatregelen. Voor slechts <strong>€9.95 per maand</strong> verzorgt
-      Horecalijst de correcte registratie, opslag (én automatische vernietiging)
-      van de klantgegevens.
-    </p>
-    <p>
-      U vraagt uw klanten bij het binnenkomen hun gegevens in te vullen bij
-      Horecalijst. Dit kan eenvoudigweg via hun eigen smartphone. Wordt uw zaak
-      gecontacteerd in het kader van een officieel contactonderzoek? Horecalijst
-      zorgt voor een geautomatiseerde export van de nodige gegevens, zonder de
-      privacy van uw klanten in het gedrang te brengen.
-    </p>
+const HowItWorks = () => {
+  const { data: productData } = useQuery(
+    gql`
+      query product($period: String!) {
+        product(period: $period) {
+          value
+        }
+      }
+    `,
+    {
+      variables: { period: 'MONTHLY' },
+    },
+  );
+  const price = useMemo(() => {
+    return productData?.product?.value || null;
+  }, [productData?.product?.value]);
 
-    <div className={styles.cta}>
-      <Link href="/zakelijk">
-        <a>Probeer het gratis!</a>
-      </Link>
+  return (
+    <div className={styles.howItWorks}>
+      <h2 id="hoe-het-werkt">Hoe werkt het?</h2>
+      <p>
+        Voor u als horeca-uitbater is het bijhouden van de contactgegevens van
+        uw klanten de nieuwste in een steeds langer wordende lijst van
+        coronamaatregelen. Voor slechts <strong>€{price} per maand</strong>{' '}
+        verzorgt Horecalijst de correcte registratie, opslag (én automatische
+        vernietiging) van de klantgegevens.
+      </p>
+      <p>
+        U vraagt uw klanten bij het binnenkomen hun gegevens in te vullen bij
+        Horecalijst. Dit kan eenvoudigweg via hun eigen smartphone. Wordt uw
+        zaak gecontacteerd in het kader van een officieel contactonderzoek?
+        Horecalijst zorgt voor een geautomatiseerde export van de nodige
+        gegevens, zonder de privacy van uw klanten in het gedrang te brengen.
+      </p>
+
+      <div className={styles.cta}>
+        <Link href="/zakelijk">
+          <a>Probeer het gratis!</a>
+        </Link>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default HowItWorks;
